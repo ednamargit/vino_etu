0b56da92440d475db8ae17c2d629b40b1f84005c
Chingiz Taghizade <t.chingiz@gmail.com>
2020-10-09 18:28:59 -0400

    Conflits d'authentification 2

--- vues/nouveauUtilisateur.php
+++ vues/nouveauUtilisateur.php
@@ -5,19 +5,19 @@
         <?php header('Location: index.php'); ?>
         <?php endif;?>
         <p> <input name="date_inscription" type="hidden" id="dateActuelle">
-            <span>Nom</span><input class="inputForm" name="nom" placeholder="Entre 2 et 25 caractères alphabétiques M ou m" required>
+            <span>Nom</span><input class="inputForm" name="nom" placeholder="Entre 2 et 25 caractères ou chiffres M ou m" required>
             <span class="erreur"><?= isset($erreurs['nom']) ? $erreurs['nom'] : "" ?></span>
         </p>
         <p>
-            <span>Prénom</span><input class="inputForm" name="prenom" placeholder="Entre 2 et 25 caractères alphabétiques M ou m" required>
+            <span>Prénom</span><input class="inputForm" name="prenom" placeholder="Entre 2 et 25 caractères ou chiffres M ou m" required>
             <span class="erreur"> <?= isset($erreurs['prenom']) ? $erreurs['prenom'] : "" ?></span>
         </p>
         <p>
-            <span>Identifiant</span> <input class="inputForm" name="identifiant" placeholder="Entre 3 et 16 caractères alphabétiques M ou m" required>
+            <span>Identifiant</span> <input class="inputForm" name="identifiant" placeholder="Entre 3 et 16 caractères ou chiffres M ou m" required>
             <span class="erreur"><?= isset($erreurs['identifiant']) ? $erreurs['identifiant'] : "" ?></span>
         </p>
         <p>
-            <span>Mot de passe</span> <input class="inputForm" type="password" name="mdp" placeholder="8 caractères ou plus M ou m" required>
+            <span>Mot de passe</span> <input class="inputForm" type="password" name="mdp" placeholder="8 caractères,chiffres ou plus M ou m" required>
             <span class="erreur"><?= isset($erreurs['mdp']) ? $erreurs['mdp'] : "" ?></span>
         </p>
         <p><button type="submit" class="creationButton" name="creer" id='creerUsager'>Créez</button></p>
